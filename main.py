f64197d672103dcf8e4c545cd71e7306b93e496a
Previn Wong <previnwong@Previns-MacBook-Air.local>
2019-06-17 10:32:27 -0700

    testing

--- main.py
+++ main.py
@@ -1,18 +1,69 @@
-from flask import Flask, request, json, jsonify
-import os
-import webbrowser
+from flask import Flask, jsonify, abort, make_response, request, url_for, json
+import json
 
 app = Flask(__name__)
 
+data = {
+    "contact_phone_number": "+14086494479",
+    "call_id": '123',
+    "url": "https://app.procedureflow.com"
+}
+
 @app.route('/', methods=['POST'])
 def webhook():
-	if request.headers['Content-Type'] == 'application/json':
-		my_info = json.dumps(request.json)
-		web = '{"url": "https://app.procedureflow.com"}'
-		print(my_info)
-		print(web)
+    if request.headers['Content-Type'] == 'application/json':
+        info = json.dumps(request.json)
+        info2 = json.loads(info)
+        # print(info2)
+        print(info2["contact_phone_number"])
+        print(info2["call_id"])
+        print(info2["url"])
+        post_data = {
+            "contact_phone_number": info2["contact_phone_number"],
+            "call_id": info2["call_id"],
+            "url": info2["url"]
+        }
+        print(post_data)
+        return jsonify(post_data), 201
 
-		return web
+@app.route('/get', methods=['GET'])
+def get_tasks():
+    return jsonify(data)
+
+@app.route('/post', methods=['GET'])
+def create_task():
+    if not request.json:
+        abort(400)
+    test = {
+        "contact_phone_number": request.json["contact_phone_number"],
+        "call_id": request.json["call_id"],
+        "url": request.json["url"]
+    }
+    print(test)
+    return jsonify(test), 201
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
+
+# from flask import Flask, request, json, jsonify
+# import os
+# import webbrowser
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['POST'])
+# def webhook():
+# 	if request.headers['Content-Type'] == 'application/json':
+# 		my_info = json.dumps(request.json)
+# 		web = '{"url": "https://app.procedureflow.com"}'
+# 		print(my_info)
+# 		print(web)
+
+# 		return web
+
+# if __name__ == '__main__':
+# 	app.run(debug=True)
--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
-
 header('Content-Type: application/json');
 $request = file_get_contents('php://input');
 $req_dump = print_r( $request, true );
-$fp = file_put_contents( 'request.html', $req_dump );
+$fp = file_put_contents( 'request.log', $req_dump );
+$arr = array('screenpop' => 'app.procedureflow.com');
+$stats = json_encode($req_dump);
+echo json_encode($arr);
 ?>
