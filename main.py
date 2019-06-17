715117868adac1cb49b08004eaeb59625b139562
Previn Wong <previnwong@Previns-MacBook-Air.local>
2019-06-17 11:35:43 -0700

    test

--- main.py
+++ main.py
@@ -5,35 +5,8 @@ import json
 
 app = Flask(__name__)
 
-data = {
-    "contact_phone_number": "+14086494479",
-    "call_id": '123',
-    "url": "https://app.procedureflow.com"
-}
-
-# @app.route('/', methods=['POST'])
-# def webhook():
-#     if request.headers['Content-Type'] == 'application/json':
-#         info = json.dumps(request.json)
-#         info2 = json.loads(info)
-#         # print(info2)
-#         print(info2["contact_phone_number"])
-#         print(info2["call_id"])
-#         print(info2["url"])
-#         post_data = {
-#             "contact_phone_number": info2["contact_phone_number"],
-#             "call_id": info2["call_id"],
-#             "url": info2["url"]
-#         }
-#         print(post_data)
-#         return jsonify(post_data), 201
-
-@app.route('/get', methods=['GET'])
-def get_tasks():
-    return jsonify(data)
-
-@app.route('/post', methods=['POST'])
-def create_task():
+@app.route('/', methods=['POST'])
+def webhook():
     if not request.json:
         abort(400)
     test = {
@@ -50,22 +23,3 @@ def not_found(error):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# from flask import Flask, request, json, jsonify
-# import os
-# import webbrowser
-
-# app = Flask(__name__)
-
-# @app.route('/', methods=['POST'])
-# def webhook():
-# 	if request.headers['Content-Type'] == 'application/json':
-# 		my_info = json.dumps(request.json)
-# 		web = '{"url": "https://app.procedureflow.com"}'
-# 		print(my_info)
-# 		print(web)
-
-# 		return web
-
-# if __name__ == '__main__':
-# 	app.run(debug=True)
