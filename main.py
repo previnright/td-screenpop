d1550ab95edccd2ac567d1ff70ce2f27641651bc
Previn Wong <previnwong@Previns-MacBook-Air.local>
2019-06-17 18:35:24 -0700

    socket

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 #Created by: Phillip Zeelig
 
-from flask import Flask, jsonify, abort, make_response, request, url_for, json
+from flask import Flask, jsonify, abort, make_response, request, url_for, json, render_template, session
+from flask_socketio import SocketIO, send, emit
 import json
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+# app.config.from_object('config')
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True, namespace = '/')
+
+# @app.route('/socket')
+# def socket():
+# 	return render_template('data.html')
 
 @app.route('/', methods=['POST', 'GET'])
 def webhook():
@@ -17,6 +30,11 @@ def webhook():
 	        "url": request.json["url"]
 	    }
 	    print(test)
+	    test2 = json.dumps(test)
+	    test3 = str(test)
+	    handleMessage(test3)
+	    # socketio.emit('my response', test2)
+	    # print('Message: ' + test2)
 	    return jsonify(test), 201
 
 	else:
@@ -27,5 +45,7 @@ def webhook():
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    socketio.run(app)
